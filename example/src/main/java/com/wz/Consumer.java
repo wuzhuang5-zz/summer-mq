5bab7b7716b75314c9b95dde5eec05893162f651
wuzhuang <wz@wuzhuangdeMacBook-Pro.local>
2020-01-08 17:50:19 +0800

    example

---
+++ example/src/main/java/com/wz/Consumer.java
@@ -0,0 +1,32 @@
+package com.wz;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @Author: wz
+ */
+public class Consumer {
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("消费组名");
+        consumer.setNamesrvAddr("localhost:9876");
+        consumer.subscribe("topic", "*");
+        //Register callback to execute on arrival of messages fetched from brokers.
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
--- example/src/main/java/com/wz/SyncProducer.java
+++ example/src/main/java/com/wz/SyncProducer.java
@@ -1,12 +1,22 @@
 package com.wz;
 
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
 
 /**
  * @Author: wz
  */
 public class SyncProducer {
     public static void main(String[] args) throws Exception{
-        DefaultMQProducer
+        DefaultMQProducer producer = new DefaultMQProducer("生产组名");
+        producer.setNamesrvAddr("localhost:9876");
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            Message message = new Message("topic", "tag", ("Hello World"+i).getBytes());
+            SendResult result = producer.send(message);
+            System.out.printf("%s%n", result);
+        }
+        producer.shutdown();
     }
 }
