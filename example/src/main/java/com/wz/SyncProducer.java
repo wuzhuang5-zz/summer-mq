7a0659cd0d9ee09c8fa2116bcaad0be2c0dba5b9
wuzhuang <wz@wuzhuangdeMacBook-Pro.local>
2020-01-08 16:16:18 +0800

    example

---
+++ example/src/main/java/com/wz/SyncProducer.java
@@ -0,0 +1,12 @@
+package com.wz;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+
+/**
+ * @Author: wz
+ */
+public class SyncProducer {
+    public static void main(String[] args) throws Exception{
+        DefaultMQProducer
+    }
+}
